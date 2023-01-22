0de758ef1b584c44301f5c3c25e7237d7c65baf5
Dalibor Trivundza <dtrivundza1977@gmail.com@>
2023-01-22 16:55:22 +0100

    Failovi za vjezbu

---
+++ 08022023/tablica mnozenja.py
@@ -0,0 +1,5 @@
+print"************"
+
+print(1*1, end="\t")
+print(1*2, end="\t")
+print(1*3,end="\n")
---
+++ 08022023/while.py
@@ -0,0 +1,50 @@
+import random
+#sekunde=10
+#if sekunde>0:
+ #   print(sekunde)
+  #  sekunde-=1
+#if sekunde>0:
+   # print(sekunde)  
+  #  sekunde-=1
+#if sekunde>0:
+ #   print(sekunde)
+
+#sekunde=10
+#while sekunde>0:
+#    print(sekunde)
+#    sekunde-=1
+#while else:
+#    print("Cao")
+
+
+#baterija=90
+#while baterija>0:
+  #print("mozes koristiti telefon", baterija,"%")
+  #baterija= random.randint(5,20)
+
+
+
+
+
+#for broj in range(1,10):
+#  if broj==5:
+#    break
+#  print(broj)
+
+#for broj in range(10):
+#  if broj==2:
+ #   continue
+ # print(broj)
+
+#sekunde=10
+#while sekunde>0:
+# sekunde-=1
+#  print( sekunde)
+
+baterija=90
+while baterija>0:
+  #print("Mozete koristiti telefon:", baterija, "%")
+  #baterija -=random.randint(5,20)
+
+  print("Baterija je prazna.")
+
